ecba870e06e6184f18518ce9660bfb767839669f
saeedurrehman01 <saeedrehman241@gmail.com>
2024-07-23 23:23:13 +0500

    Card added

---
+++ NextJs Practice/hello-world/app/components/button/Button.tsx
@@ -0,0 +1,5 @@
+export default function Button (){
+    return(
+        <button>Submit</button>
+    )
+}
---
+++ NextJs Practice/hello-world/app/components/card/Card.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+interface CardProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+
+const Card: React.FC<CardProps> = ({ title, description, imageUrl }) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <Image
+        className="w-full"
+        src={imageUrl}
+        alt={title}
+        width={400}
+        height={225}
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
